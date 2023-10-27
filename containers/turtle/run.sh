ab298bdfea22305df2f0538c5e37370c481eef45
Onno Huijgen <OnnoH@users.noreply.github.com>
2023-10-27 22:50:26 +0200

    Added Docker container creation (#25)
    for running the Python Turtle output

---
+++ containers/turtle/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CHANNEL=${1}
+
+BROKER_HOST=192.168.1.73
+BROKER_PORT=1883
+
+DISPLAY=host.docker.internal:0
+
+sed 's/@CHANNEL@/'${CHANNEL}'/; s/@BROKER_HOST@/'${BROKER_HOST}'/; s/@BROKER_PORT@/'${BROKER_PORT}'/;' turtle.properties > turtle-${CHANNEL}.properties
+
+docker run --rm --user $(id -u $USER):$(id -g $USER) \
+           --env DISPLAY=$DISPLAY \
+           --volume ./turtle-${CHANNEL}.properties:/turtle.properties \
+           vincent-turtle
